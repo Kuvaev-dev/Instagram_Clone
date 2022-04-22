7417aaa6c0c2250951cc30e3b728650750a7d353
Nikita Kuvaev <likeaboss13371@gmail.com>
2022-04-22 19:32:55 +0300

    Bottom Navigation UI Added

--- app/src/main/java/com/mainapp/instagramclone/HomeActivity.java
+++ app/src/main/java/com/mainapp/instagramclone/HomeActivity.java
@@ -3,12 +3,28 @@ package com.mainapp.instagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.mainapp.instagramclone.Utils.BottomNavigationViewHelper;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: starting.");
+        setupBottomNavigationView();
+    }
+
+    /*
+     * Bottom Navigation View Setup
+     */
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "SetUpBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }
---
+++ app/src/main/java/com/mainapp/instagramclone/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,17 @@
+package com.mainapp.instagramclone.Utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx) {
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
