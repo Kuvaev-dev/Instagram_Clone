ab069f22573efcf327b314419b9fbb4aaf2717dc
Nikita Kuvaev <likeaboss13371@gmail.com>
2022-04-23 16:44:56 +0300

    Profile Toolbar And Menu Added

--- app/src/main/java/com/mainapp/instagramclone/Profile/ProfileActivity.java
+++ app/src/main/java/com/mainapp/instagramclone/Profile/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.mainapp.instagramclone.R;
@@ -19,9 +20,25 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
+        setupToolBar();
+    }
+
+    private void setupToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(menuItem -> {
+            Log.d(TAG, "onMenuItemClick: clicked menu item: " + menuItem);
+
+            switch (menuItem.getItemId()) {
+                case R.id.profileMenu:
+                    Log.d(TAG, "onMenuItemClick: Navigating to profile preferences");
+            }
+
+            return false;
+        });
     }
 
     /*
@@ -35,5 +52,11 @@ public class ProfileActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
