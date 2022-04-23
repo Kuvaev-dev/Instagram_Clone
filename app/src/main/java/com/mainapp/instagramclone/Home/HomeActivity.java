b1eb750adc17c953850e1e5745ec00606a12bce5
Nikita Kuvaev <likeaboss13371@gmail.com>
2022-04-23 16:08:04 +0300

    Section Pager Adapter Tabs Added

--- app/src/main/java/com/mainapp/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/com/mainapp/instagramclone/Home/HomeActivity.java
@@ -1,15 +1,19 @@
 package com.mainapp.instagramclone.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.tabs.TabLayout;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.mainapp.instagramclone.R;
 import com.mainapp.instagramclone.Utils.BottomNavigationViewHelper;
+
+import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
@@ -20,7 +24,30 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: starting.");
+
         setupBottomNavigationView();
+        setupViewPager();
+    }
+
+    /*
+     * Responsible for adding the 3 tabs: Camera, Home, Messages
+     */
+    private void setupViewPager() {
+        SectionPagerAdapter sectionPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager());
+
+        sectionPagerAdapter.addFragment(new CameraFragment());
+        sectionPagerAdapter.addFragment(new HomeFragment());
+        sectionPagerAdapter.addFragment(new MessagesFragment());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(sectionPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        Objects.requireNonNull(tabLayout.getTabAt(0)).setIcon(R.drawable.ic_camera);
+        Objects.requireNonNull(tabLayout.getTabAt(1)).setIcon(R.drawable.ic_action_menu);
+        Objects.requireNonNull(tabLayout.getTabAt(2)).setIcon(R.drawable.ic_arrow);
     }
 
     /*
