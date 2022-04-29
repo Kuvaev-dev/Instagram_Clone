ba394a46429471fcb563294f6decca1b700a6e2b
Nikita Kuvaev <likeaboss13371@gmail.com>
2022-04-29 17:59:07 +0300

    Retrieving User Data From Database Added

--- app/src/main/java/com/mainapp/instagramclone/Profile/ProfileFragment.java
+++ app/src/main/java/com/mainapp/instagramclone/Profile/ProfileFragment.java
@@ -19,7 +19,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.mainapp.instagramclone.Login.LoginActivity;
 import com.mainapp.instagramclone.R;
 import com.mainapp.instagramclone.Utils.BottomNavigationViewHelper;
 import com.microprogramer.library.CircularImageView;
@@ -32,6 +40,12 @@ public class ProfileFragment extends Fragment {
     private ImageView profileMenu;
     private Context context;
     private BottomNavigationViewEx bottomNavigationViewEx;
+
+    // Firebase
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Nullable
     @Override
@@ -79,4 +93,53 @@ public class ProfileFragment extends Fragment {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
+
+    // Firebase BEGINNING
+
+    /*
+     *  Setup the firebase auth object
+     */
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+        auth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        authStateListener = firebaseAuth -> {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+            if (user != null)
+                Log.d(TAG, "onAuthStateChanged: signed in: " + user.getUid());
+            else
+                Log.d(TAG, "onAuthStateChanged: signed out.");
+        };
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Retrieve user information from the database
+
+                // Retrieve images for the user in question
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authStateListener != null)
+            auth.removeAuthStateListener(authStateListener);
+    }
+
+    // Firebase END
 }
