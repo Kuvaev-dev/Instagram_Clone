8429818995b5d28b83d23672cd747550cba0afca
Nikita Kuvaev <likeaboss13371@gmail.com>
2022-05-03 17:29:04 +0300

    Gallery Fragment Layout Added

--- app/src/main/java/com/mainapp/instagramclone/Share/GalleryFragment.java
+++ app/src/main/java/com/mainapp/instagramclone/Share/GalleryFragment.java
@@ -5,6 +5,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,10 +20,34 @@ import com.mainapp.instagramclone.R;
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment";
 
+    private GridView gridView;
+    private ImageView galleryImage;
+    private ProgressBar progressBar;
+    private Spinner directorySpinner;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
         Log.d(TAG, "onCreateView: started.");
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        galleryImage = view.findViewById(R.id.galleryImageView);
+        gridView = view.findViewById(R.id.gridView);
+        directorySpinner = view.findViewById(R.id.spinnerDirectory);
+        progressBar = view.findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+
+        ImageView shareClose = view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(view1 -> {
+            Log.d(TAG, "onCreateView: closing the gallery fragment.");
+            getActivity().finish();
+        });
+
+        TextView nextScreen = view.findViewById(R.id.tvNext);
+        nextScreen.setOnClickListener(view2 -> {
+            Log.d(TAG, "onCreateView: navigating to the final share screen.");
+
+        });
+
+        return view;
     }
 }
