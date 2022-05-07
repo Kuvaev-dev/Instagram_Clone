c76bf1a98fcd7858d8062cdf8b4a46934e109a54
Nikita Kuvaev <likeaboss13371@gmail.com>
2022-05-07 17:05:53 +0300

    Retrieving Post Image From Bundle Added

--- app/src/main/java/com/mainapp/instagramclone/Post/ViewPostFragment.java
+++ app/src/main/java/com/mainapp/instagramclone/Post/ViewPostFragment.java
@@ -1,20 +1,97 @@
 package com.mainapp.instagramclone.Post;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.mainapp.instagramclone.Models.Photo;
 import com.mainapp.instagramclone.R;
+import com.mainapp.instagramclone.Utils.BottomNavigationViewHelper;
+import com.mainapp.instagramclone.Utils.SquareImageView;
+import com.mainapp.instagramclone.Utils.UniversalImageLoader;
 
 public class ViewPostFragment extends Fragment {
+    private static final String TAG = "ViewPostFragment";
+
+    private BottomNavigationViewEx bottomNavigationViewEx;
+    private TextView mBackLabel, mCaption, mUsername, mTimestamp;
+    private ImageView mBackArrow, mEllipses, mHeartRed, mHeartWhite, mProfileImage;
+
+    private int mActivityNum = 0;
+
+    public ViewPostFragment() {
+        super();
+        setArguments(new Bundle());
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_view_post, container, false);
+        SquareImageView mPostImage = view.findViewById(R.id.post_image);
+        bottomNavigationViewEx = view.findViewById(R.id.bottomNavViewBar);
+        mBackArrow = view.findViewById(R.id.backArrow);
+        mBackLabel = view.findViewById(R.id.tvBackLabel);
+        mCaption = view.findViewById(R.id.image_caption);
+        mUsername = view.findViewById(R.id.username);
+        mTimestamp = view.findViewById(R.id.image_time_posted);
+        mEllipses = view.findViewById(R.id.ivEllipses);
+        mHeartRed = view.findViewById(R.id.image_heart_red);
+        mHeartWhite = view.findViewById(R.id.image_heart);
+        mProfileImage = view.findViewById(R.id.profile_photo);
+
+        try {
+            Photo mPhoto = getPhotoFromBundle();
+            assert mPhoto != null;
+            UniversalImageLoader.setImage(mPhoto.getImage_path(), mPostImage, null, "");
+            mActivityNum = getActivityNumFromBundle();
+        } catch (NullPointerException exception) {
+            Log.e(TAG, "onCreateView: NullPointerException: " + exception.getMessage());
+        }
+
+        setupBottomNavigationView();
+
+        return view;
+    }
+
+    private int getActivityNumFromBundle() {
+        Log.d(TAG, "getActivityNumFromBundle: arguments: " + getArguments());
+        Bundle bundle = this.getArguments();
+        if (bundle != null)
+            return bundle.getInt(getString(R.string.activity_number));
+        else
+            return 0;
+    }
+
+    private Photo getPhotoFromBundle() {
+        Log.d(TAG, "getPhotoFromBundle: arguments: " + getArguments());
+        Bundle bundle = this.getArguments();
+        if (bundle != null)
+            return bundle.getParcelable(getString(R.string.photo));
+        else
+            return null;
+    }
+
+    /*
+     * Bottom Navigation View Setup
+     */
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "SetUpBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(getActivity(), getActivity(), bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(mActivityNum);
+        menuItem.setChecked(true);
     }
 }
